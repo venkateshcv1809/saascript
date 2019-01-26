75e3f05c629369dd224d79d73b79298f592f2cf3
VENKATESH C.V <venkateshvairam@gmail.com>
2019-01-27 01:25:46 +0530

    Basic Project configuration and express server setup

---
+++ src/App.ts
@@ -0,0 +1,24 @@
+import express from "express";
+
+class App {
+    public express: express.Express;
+
+    constructor() {
+        this.express = express();
+        this.mountRoutes();
+    }
+
+    private mountRoutes(): void {
+        const router = express.Router();
+
+        router.get("/", (req, res) => {
+            res.json({
+                message: "Welcome!"
+            });
+        });
+
+        this.express.use("/", router);
+    }
+}
+
+export default new App().express;
