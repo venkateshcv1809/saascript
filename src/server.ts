654bfda414a8169f9cc01daec283ad97d1d6ae9f
VENKATESH C.V <venkateshvairam@gmail.com>
2019-12-04 02:23:00 +0530

    Updated npm modules

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,9 @@
+import logger from "winston";
 import app from "./App";
 
 const port = process.env.PORT || 8080;
 
-app.listen(port, (err: any) => {
-    if (err) {
-        return err;
-    }
-
-    return `server is listening on ${port}`;
+app.listen(port, () => {
+    logger.debug(`server is listening on ${port}`);
+    return;
 });
